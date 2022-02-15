d241a4dbffbf28caa3ef536a558defad47950db4
Rick Rackley <93223405+safehaven1017@users.noreply.github.com>
2022-02-15 12:27:56 -0500

    setting up redux

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import { BrowserRouter } from 'react-router-dom';
+
+// Holiday API Key: bce2a4a9-bea5-4359-a477-7e9747f7e3f9
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
